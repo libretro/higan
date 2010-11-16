850474b39e11ab4bde64060c944a51169372086e
Tim Allen <screwtape@froup.com>
2010-11-16 23:16:58 +1100

    Make libao buffer output.
    This implments the 'latency' option, and also works around libao's ALSA
    driver making a syscall for every single sample pair, a crippling
    slow-down.

--- bsnes/ruby/audio/ao.cpp
+++ bsnes/ruby/audio/ao.cpp
@@ -1,9 +1,10 @@
 /*
-  audio.ao (2010-11-13)
+  audio.ao (2010-11-15)
   authors: Nach, RedDwarf
   contributor: Kernigh
 */
 
+#include <stdio.h> // snprintf
 #include <ao/ao.h>
 
 namespace ruby {
@@ -13,16 +14,25 @@ public:
   ao_device *audio_device;
 
   struct {
+    int16_t *data;  // signed 16-bit samples, native byte order
+    unsigned index;
+    unsigned size;
+  } buffer;
+
+  struct {
     unsigned frequency;
+    unsigned latency;
   } settings;
 
   bool cap(const string& name) {
     if(name == Audio::Frequency) return true;
+    if(name == Audio::Latency) return true;
     return false;
   }
 
   any get(const string& name) {
     if(name == Audio::Frequency) return settings.frequency;
+    if(name == Audio::Latency) return settings.latency;
     return false;
   }
 
@@ -33,15 +43,22 @@ public:
       return true;
     }
 
+    if(name == Audio::Latency) {
+      settings.latency = any_cast<unsigned>(value);
+      if(audio_device) init();
+      return true;
+    }
+
     return false;
   }
 
   void sample(uint16_t l_sample, uint16_t r_sample) {
-    uint16_t samp[2];
-    samp[0] = l_sample;
-    samp[1] = r_sample;
+    buffer.data[buffer.index++] = l_sample;
+    buffer.data[buffer.index++] = r_sample;
+    if(buffer.index < buffer.size) return;
 
-    ao_play(audio_device, (char *)samp, sizeof samp);
+    ao_play(audio_device, (char *)buffer.data, buffer.size * sizeof(*buffer.data));
+    buffer.index = 0;
   }
 
   void clear() {
@@ -65,16 +82,30 @@ public:
     ao_info *di = ao_driver_info(driver_id);
     if(!di) return false;
     if(!strcmp(di->short_name, "alsa")) {
-      ao_append_option(&options, "buffer_time", "80"); //80ms latency (default was 500ms)
+      char latency_str[32];
+
+      // reduce latency (default was 500ms)
+      snprintf(latency_str, sizeof latency_str, "%u", settings.latency);
+      ao_append_option(&options, "buffer_time", latency_str);
     }
 
     audio_device = ao_open_live(driver_id, &driver_format, options);
     if(!audio_device) return false;
 
+    // (2 samples per stereo frame) * (frames per second) * (seconds of latency)
+    buffer.size = 2 * unsigned(settings.frequency * settings.latency / 1000.0 + 0.5);
+    buffer.data = new int16_t[buffer.size];
+    buffer.index = 0;
+
     return true;
   }
 
   void term() {
+    if(buffer.data) {
+      delete[] buffer.data;
+      buffer.data = 0;
+    }
+
     if(audio_device) {
       ao_close(audio_device);
       audio_device = 0;
@@ -85,7 +116,10 @@ public:
     audio_device = 0;
     ao_initialize();
 
+    buffer.data = 0;
+
     settings.frequency = 22050;
+    settings.latency = 80;
   }
 
   ~pAudioAO() {
